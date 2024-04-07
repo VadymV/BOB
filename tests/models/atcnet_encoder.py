2ba9af6fa5adf8948df7c7e04fabb70fddd3f223
Vadym Gryshchuk <vagr@di.ku.dk>
2024-04-07 23:15:52 +0200

    Missing doc.

--- tests/models/atcnet_encoder.py
+++ tests/models/atcnet_encoder.py
@@ -7,11 +7,18 @@ import torch
 
 from bob.models.atcnet_encoder import ATCNetEncoder
 
+
 @pytest.fixture
 def my_atcnet_encoder():
-    '''Returns a ATCNetEncoder instance'''
+    """
+    Creates an instance of the ATCNetEncoder class.
+    Returns:
+        An instance of the ATCNetEncoder class.
+    """
+
     return ATCNetEncoder(num_electrodes=28, chunk_size=1125)
 
 
 def test_atcnet_encoder(my_atcnet_encoder):
-    assert my_atcnet_encoder(torch.randn(32, 1, 28, 1125)).shape == torch.Size([32, 32])
+    assert my_atcnet_encoder(torch.randn(32, 1, 28, 1125)).shape == torch.Size(
+        [32, 32])
